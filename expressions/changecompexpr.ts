f0280849801829ddf513395ff2b9b41d22ec8145
bandaloo <bandaloo@icloud.com>
2020-06-18 16:36:42 -0400

    error checking for get and change components

--- expressions/changecompexpr.ts
+++ expressions/changecompexpr.ts
@@ -1,5 +1,6 @@
 import { AllVals, Vec } from "../exprtypes";
 import { Operator, wrapInValue } from "./expr";
+import { typeStringToLength, checkLegalComponents } from "./getcompexpr";
 
 // test with just setting
 function getChangeFunc(
@@ -14,8 +15,34 @@ function getChangeFunc(
 }`;
 }
 
+// illegal when:
+
+// duplicate components
+
+// setter has different length than components
+
+// components are illegal
+
+function checkGetComponents(comps: string, setter: AllVals, vec: Vec) {
+  // setter has different length than components
+  if (comps.length !== typeStringToLength(setter.typeString())) {
+    throw new Error("components length must be equal to the target float/vec");
+  }
+  // duplicate components
+  if (duplicateComponents(comps)) {
+    throw new Error("duplicate components not allowed on left side");
+  }
+  // legal components
+  checkLegalComponents(comps, vec);
+}
+
+function duplicateComponents(comps: string) {
+  return new Set(comps.split("")).size !== comps.length;
+}
+
 export class ChangeCompExpr<T extends Vec> extends Operator<T> {
   constructor(vec: T, setter: AllVals, comps: string) {
+    checkGetComponents(comps, setter, vec);
     /** random hash to name a custom function */
     const hash = Math.random().toString(36).substring(5);
     console.log(hash);
--- expressions/getcompexpr.ts
+++ expressions/getcompexpr.ts
@@ -1,5 +1,19 @@
-import { Vec, Vec3, Vec2, Vec4 } from "../exprtypes";
+import { Vec, Vec3, Vec2, Vec4, TypeString } from "../exprtypes";
 import { ExprFloat, ExprVec2, ExprVec3, ExprVec4, SourceLists } from "./expr";
+
+// TODO this should probably be somewhere else
+export function typeStringToLength(str: TypeString) {
+  switch (str) {
+    case "float":
+      return 1;
+    case "vec2":
+      return 2;
+    case "vec3":
+      return 3;
+    case "vec4":
+      return 4;
+  }
+}
 
 function genCompSource(vec: Vec, components: string): SourceLists {
   return {
@@ -8,31 +22,56 @@ function genCompSource(vec: Vec, components: string): SourceLists {
   };
 }
 
+export function checkLegalComponents(comps: string, vec: Vec) {
+  const check = (range: string, domain: string) => {
+    let inside = 0;
+    let outside = 0;
+    for (const c of range) {
+      domain.includes(c) ? inside++ : outside++;
+    }
+    return inside === inside && !outside;
+  };
+
+  const inLen = typeStringToLength(vec.typeString());
+  const rgbaCheck = check(comps, "rgba".substr(0, inLen));
+  const xyzwCheck = check(comps, "xyzw".substr(0, inLen));
+  if (!(rgbaCheck || xyzwCheck)) {
+    throw new Error("component sets are mixed or incorrect entirely");
+  }
+}
+
+function checkGetComponents(comps: string, outLen: number, vec: Vec) {
+  if (comps.length > outLen) throw new Error("too many components");
+  checkLegalComponents(comps, vec);
+}
+
 export class GetCompExpr extends ExprFloat {
   constructor(vec: Vec, comps: string) {
+    checkGetComponents(comps, 1, vec);
     super(genCompSource(vec, comps), ["uVec"]);
   }
 }
 
 export class Get2CompExpr extends ExprVec2 {
   constructor(vec: Vec2 | Vec3 | Vec4, comps: string) {
+    checkGetComponents(comps, 2, vec);
     super(genCompSource(vec, comps), ["uVec"]);
   }
 }
 
 export class Get3CompExpr extends ExprVec3 {
   constructor(vec: Vec3 | Vec4, comps: string) {
+    checkGetComponents(comps, 3, vec);
     super(genCompSource(vec, comps), ["uVec"]);
   }
 }
 
 export class Get4CompExpr extends ExprVec4 {
   constructor(vec: Vec | Vec4, comps: string) {
+    checkGetComponents(comps, 4, vec);
     super(genCompSource(vec, comps), ["uVec"]);
   }
 }
-
-// TODO error checking for correct components
 
 export function getcomp(vec: Vec, comps: string) {
   return new GetCompExpr(vec, comps);
